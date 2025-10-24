0f0442b7596820471eb29da0947bf687aa4e2c4c
harmain1251 <harmain232005@gmail.com>
2025-10-24 12:05:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 import random
 
 app = Flask(__name__)
+CORS(app)  # allows HTML pages from other origins to access this API
 
+# List of quotes
 quotes = [
     "The best way to get started is to quit talking and begin doing. – Walt Disney",
     "Don’t let yesterday take up too much of today. – Will Rogers",
@@ -13,9 +16,13 @@ quotes = [
     "With the new day comes new strength and new thoughts. – Eleanor Roosevelt"
 ]
 
-@app.route('/quote', methods=['GET'])
-def get_quote():
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/quote')
+def quote():
     return jsonify({"quote": random.choice(quotes)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
